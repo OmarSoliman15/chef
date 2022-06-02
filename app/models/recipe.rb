9fd09b295d60e9e7419e42677ba620f15a262e1b
oSoliman <omarwaityou@gmail.com>
2022-06-02 02:54:29 +0200

    Ingredients filters backend

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ApplicationRecord
+  scope :filter_by_ingredient, -> (ingredient) { where("LOWER(array_to_string(ingredients, ', ')) like ?",  ingredient) }
 end
--- db/migrate/20220531213710_add_ingredients_to_recipe.rb
+++ db/migrate/20220531213710_add_ingredients_to_recipe.rb
@@ -1,5 +1,6 @@
 class AddIngredientsToRecipe < ActiveRecord::Migration[7.0]
   def change
     add_column :recipes, :ingredients, :text, array: true, default: []
+    add_index :recipes, :ingredients, using: :gin
   end
 end
